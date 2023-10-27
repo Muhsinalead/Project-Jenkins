d27bf6a4caf68e830c20d67ad89f4e10c3037a70
Subarna <Subarna@DESKTOP-78VU4SS>
2023-10-27 22:20:58 +0600

    hiii

---
+++ Jenkins_Demo/src/test/java/Test/SampleTest.java
@@ -0,0 +1,36 @@
+package Test;
+
+import org.testng.annotations.Test;
+
+import Utilities.BaseClass;
+
+public class SampleTest extends BaseClass {
+	
+	
+  @Test
+  public void TestCase1() {
+	  
+	  System.out.println("This is test 1");
+	  
+  }
+  
+  
+  @Test
+  public void TestCase2() {
+	  
+	  System.out.println("This is test 2");
+  }
+  
+  @Test
+  public void TestCase3() {
+	  
+	  
+	  System.out.println("This is test 3");
+	  
+  }
+  
+  
+  
+  
+  
+}
